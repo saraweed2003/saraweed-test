1085afb320151a060eef2d160d234d11a7dbf6d1
sarawut.2003 <sarawutsriprachai@gmail.com>
2023-08-06 16:53:26 +0700

    .

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,5 +4,13 @@
   <div>
     <div>Follow me!</div>
     <h1><a href="https://www.youtube.com/@SARAWEED"> SARAWEED </a></h1>
+
+    <div>Product Beat</div>
+    <h1>
+      <a
+        href="https://youtube.com/playlist?list=PLSjRxfegiMSH4jFqUAgOnsMzWVnnmPHnw"
+        >SWP BEATZ
+      </a>
+    </h1>
   </div>
 </template>
