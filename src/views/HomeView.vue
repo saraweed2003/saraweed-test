e378d87d7226257ebcf46dd82e4bb719ecef048a
sarawut.2003 <sarawutsriprachai@gmail.com>
2023-08-08 07:52:56 +0700

    update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,10 @@
 <template>
   <div>
     <div>Follow me!</div>
-    <h1><a href="https://www.youtube.com/@SARAWEED"> SARAWEED </a></h1>
+    <h1><a href="https://www.youtube.com/@SARAWEED"> SARAWEED OFFICIAL </a></h1>
+
+    <div>Follow Streaming!</div>
+    <h1><a href="https://bfan.link/SWP2003"> SARAWEED </a></h1>
 
     <div>Product Beat</div>
     <h1>
